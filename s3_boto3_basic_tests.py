ead6325978e386a2f22dc08cbd653ca6264e6796
Andy Pernsteiner <andypern@gmail.com>
2016-03-29 15:27:56 -0400

    finally figured out iterating through methods..

--- s3_boto3_basic_tests.py
+++ s3_boto3_basic_tests.py
@@ -7,6 +7,7 @@ import time
 import os
 import sys
 import getopt
+import re
 from string import ascii_uppercase
 from random import choice
 from botocore.exceptions import ClientError
@@ -108,9 +109,6 @@ def list_buckets():
 	#attempt to list buckets
 	#
 
-	print 'list time'
-	print s3client
-
 	try:
 		response = s3client.list_buckets()
 	except ClientError as e:
@@ -118,7 +116,7 @@ def list_buckets():
 		sys.exit(0)
 
 	
-	print len(response['Buckets'])
+	#print len(response['Buckets'])
 
 	return(response)
 
@@ -150,7 +148,7 @@ def create_bucket():
 
 def head_bucket(cName):
 
-	print 'head'
+
 	
 
 	
@@ -159,31 +157,131 @@ def head_bucket(cName):
 		s3client.head_bucket(Bucket=cName)
 	except botocore.exceptions.ClientError as e:
 		error_code = int(e.response['Error']['Code'])
-		print error_code
+		#print error_code
 		print e.response
 
 
 def get_bucket_acl(cName):
 	#get the bucket acl
-
+	print "+++++++++++++++++"
 	try:
 		acl = s3client.get_bucket_acl(
 			Bucket=cName)
 		print acl
-
 
 	except botocore.exceptions.ClientError as e:
 		print e.response
 
 def list_objects(cName):		 
 	#list what's inside
-
+	print "+++++++++++++++++"
 	try:
-		response = s3client.list_objects(
+		objList = s3client.list_objects(
 			Bucket=cName,
 			MaxKeys=100)
+		print objList
 	except botocore.exceptions.ClientError as e:
 		print e.response
+
+
+
+def put_object_basic(cName):
+	#
+	#basic object put, nothing special
+	#
+	print "+++++++++++++++++"
+	objKey = "file-" + (''.join(choice(ascii_uppercase) for i in range(6)))
+
+	try:
+		response = s3client.put_object(
+			Body=b'xyz',
+			Bucket=cName,
+			Key=objKey)
+		print response
+		return objKey
+	except botocore.exceptions.ClientError as e:
+		print e.response
+
+
+def put_object_with_acl(cName):
+	#
+	#put another object, this time force the ACL to be set
+	#
+	print "+++++++++++++++++"
+	objKey = "file-acl-" + (''.join(choice(ascii_uppercase) for i in range(6)))
+
+	try:
+		response = s3client.put_object(
+			ACL='private',
+			Body=b'xyz',
+			Bucket=cName,
+			Key=objKey)
+		print response
+	except botocore.exceptions.ClientError as e:
+		print e.response
+
+def put_object_acl(objKey,cName):
+	#
+	#slightly different, we need to do this against an existing object
+	#
+	print "+++++++++++++++++"
+	objKey = "file-acl-" + (''.join(choice(ascii_uppercase) for i in range(6)))
+
+	try:
+		response = s3client.put_object_acl(
+			ACL='private',
+			AccessControlPolicy={
+		        'Grants': [
+		            {
+		                'Grantee': {
+		                    'DisplayName': 'dorkface',
+		                    'EmailAddress': 'dork@dork.com',
+		                    'Type': 'CanonicalUser',
+		                    'ID': 'apkey'
+		                },
+		                'Permission': 'FULL_CONTROL'
+		            },
+		        ],
+		        'Owner': {
+		            'DisplayName': 'igneous',
+		            'ID': 'igneous'
+		        }
+		    },
+			Bucket=cName,
+			Key=objKey)
+
+		print response
+	except botocore.exceptions.ClientError as e:
+		print e.response
+
+
+
+def get_slew_bucket_ops(cName):
+	
+	#
+	#slew of bucket operations packed into a single function for now
+	#
+
+	#first, build a list of methods
+	print "+++++++++++++++++"
+	method_list = dir(s3client)
+	# we'll want a regex to filter for the ones we want.
+	get_bucket_Re = re.compile('^get_bucket')
+	
+
+	for method in method_list:
+		if get_bucket_Re.match(method):
+			print method
+			callable_method = getattr(s3client, method)
+			try:
+				response = callable_method(
+					Bucket=cName
+					)
+				print response
+			except botocore.exceptions.ClientError as e:
+				print e.response
+
+
 
 #########
 #end of functions
@@ -198,7 +296,7 @@ s3client = make_session()
 ##This is if you want to operate against
 ##the bucket you just created..probably won't work due to ACL..
 #
-cName = create_bucket()
+#cName = create_bucket()
 #
 #
 ##this one is if you want to operate against the first 
@@ -210,14 +308,21 @@ print cName
 #
 #####
 
-#time.sleep(10)
 
 
-head_bucket(cName)
+
+#head_bucket(cName)
 get_bucket_acl(cName)
 list_objects(cName)
 
-#get ACL
+objKey = put_object_basic(cName)
+
+put_object_with_acl(cName)
+put_object_acl(objKey,cName)
+
+get_slew_bucket_ops(cName)
+
+
 
 #TODO: 
 
