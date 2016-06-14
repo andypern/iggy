678901fcaba57daad6f891d41967480c6bfea282
Andy Pernsteiner <andypern@gmail.com>
2016-06-13 17:17:47 -0700

    retry stuff

--- upload_retry.py
+++ upload_retry.py
@@ -4,7 +4,7 @@ import boto3
 import botocore
 from boto3.s3.transfer import S3Transfer, TransferConfig
 
-
+import logging
 import xml.etree
 import pprint
 from datetime import datetime
@@ -18,6 +18,13 @@ from random import choice
 
 
 
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.getLogger('requests').setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+
+
+
+
 ########
 #This is a script to put a bunch of dummy files into a a bucket.
 #Use case is primarily to run tests during a planned outage such as a Petra & Lithos upgrade.
@@ -25,11 +32,16 @@ from random import choice
 #which get returned by the server when S3liteD is UP, but some lithos are down.
 #
 
-#TODO: 
-
-# * breakout retry logic into a function to eliminate dupliation
-# * rename this file and add in a download/read function
-# * trap and handle 500 errors
+#####
+# Script connects to an S3 endpoint and attempts to upload files into a container.  You specify:
+# * endpoint connection info (igneous.company.com:80)
+# * target container/bucket
+# * how many files to upload
+# * path/name of the input file , note that it will re-use this file and upload -c <count> times
+# 
+# * it will clean up files it has created at the end
+# * optionally you can run it with the '--delete' option to *ONLY* delete (things which may be leftover from previous runs)
+#
 
 ###############
 # some variables , will make this dynamic in the future.
@@ -42,7 +54,7 @@ fileprefix = "file-"
 
 
 #
-#set defaults, can be overriden via args
+#set defaults, some can be overriden via args
 #
 
 print_verbose = False
@@ -66,8 +78,8 @@ max_retries = 5
 #
 
 try:
-        opts, args = getopt.getopt(sys.argv[1:], "h:p:a:s:b:c:dlr:v", 
-        	["host=","port=","access_key=","secret_key=","bucket=","count=",
+        opts, args = getopt.getopt(sys.argv[1:], "e:a:s:b:i:c:dlr:v", 
+        	["endpoint=","access_key=","secret_key=","bucket=","count=",
         	"delete","large","retries=","verbose"])
 except getopt.GetoptError as err:
         # print help information and exit:
@@ -78,16 +90,16 @@ except getopt.GetoptError as err:
 
 for opt, arg in opts:
 
-	if opt in ('-h', '--host'):
-		host = arg
-	if opt in ('-p', '--port'):
-		port = arg
+	if opt in ('-e', '--endpoint'):
+		endpoint = arg
 	if opt in ('-a', '--access_key'):
 		access_key = arg
 	if opt in ('-s','--secret_key'):
 		secret_key = arg
 	if opt in ('-b','--bucket'):
 		bucket = arg
+	if opt in ('-i','--input_file'):
+		input_file = arg
 	if opt in ('-c','--count'):
 		filecount = int(arg)
 	if opt in ('-d','--delete'):
@@ -104,15 +116,64 @@ for opt, arg in opts:
 
 
 #
-#we require 5 arg's, and 2 are optional.
+#we require 5 arg's, plus there are some optional ones.
 #
 
 if len(opts) < 5:
-	print "syntax is `./s3_put_test.py -h <hostname> -p 80 -a <access_key> -s <secret_key> -b bucket [-c filecount] [--delete] [--large] [-r retries] [--verbose]`"
+	print "syntax is `./upload_retry.py -e <endpoint.company.com:80> -a <access_key> -s <secret_key> -b bucket -i input_file [-c filecount] [--delete] [--large] [-r retries] [--verbose]`"
 	sys.exit(1)
 
 
 
+
+
+######Functions#####
+#
+#
+#
+
+
+def main():
+		
+	s3client = make_session()
+
+	#
+	# delete_only = don't do any uploads, just cleanup.
+	#
+	if delete_only == True:
+		print "deleting all files starting with %s/%s" %(bucket,fileprefix)
+		objList =  list_objects(s3client,bucket, fileprefix)
+		count = 0
+		for page in objList:
+			for key in page['Contents']:
+				delete_object(s3client,bucket,key['Key'])
+				count += 1
+		print "deleted %s" %(count)
+	else:
+
+		#
+		#most of the time we'll do this
+		#
+		print "putting %s files to %s"  %(filecount, bucket)
+		upload_file(s3client,bucket, filecount, fileprefix)
+
+
+		print "finished putting %s files" %(filecount)
+
+		#
+		#get a list of objects in pages, then blow them away
+		#
+
+		objList =  list_objects(s3client,bucket, fileprefix)
+
+		count = 0
+		for page in objList:
+				for key in page['Contents']:
+					delete_object(s3client,bucket,key['Key'])
+					count += 1
+
+
+		print "deleted %s" %(count)
 
 
 
@@ -121,7 +182,6 @@ def printfail(method,response):
 	#
 	#this is mainly so we have a consistent way to print 
  	#
- 	# for later: if there is the '-v' flag set by the user, print more verbose stuff.
  	#
  	if print_verbose == True:
  		print "%s,%s" % (method,response)
@@ -133,7 +193,6 @@ def printsuccess(method,response):
 	#
 	#this is mainly so we have a consistent way to print
  	#
- 	# for later: if there is the '-v' flag set by the user, print more verbose stuff.
  	#
  	if print_verbose == True:
  		print "%s,%s" % (method,response)
@@ -146,8 +205,7 @@ def printsuccess(method,response):
 
 
 	#
-	#build the client session.  Note the signature version
-	# is required to be 's3' at this time, as opposed to 's3v4'
+	#build the client session.  
 	#
 
 def make_session():
@@ -158,7 +216,7 @@ def make_session():
 	s3client = session.client('s3',
 			aws_access_key_id = access_key,
 	        aws_secret_access_key = secret_key,
-			endpoint_url=urlprefix + host + ':' + port,
+			endpoint_url=urlprefix + endpoint,
 			use_ssl=False,
 			verify=False,
 			config=boto3.session.Config(
@@ -174,16 +232,13 @@ def make_session():
 
 
 
-######Functions#####
-#
-#
-#
 
 
-def list_objects(bucket, fileprefix):		 
+
+def list_objects(s3client,bucket, fileprefix):		 
 	#
 	#note that you can only get back up to 1000 keys at a time
-	# so using pagination to try and get them all.
+	# so use pagination to  get them all.
 	#
 	method = 'list_objects'
 	try:
@@ -207,63 +262,16 @@ def list_objects(bucket, fileprefix):
 
 
 
-def put_object_lots(bucket, filecount, fileprefix):
-
-	method = 'put_object_lots'
-	global_start_time = time.time()
-	for i in range(filecount):
-		local_start_time = time.time()
-		nowtime = time.strftime("%Y-%m-%d %H:%M:%S")
-		objKey = fileprefix + '-' + str(i) + '-' + (''.join(choice(ascii_uppercase) for i in range(6))) + '.txt'
-		try:
-			response = s3client.put_object(
-			Body=b'xyz',
-			Bucket=bucket,
-			Key=objKey)
-			#printsuccess(method,response)
-			local_elapsed_time = time.time() - local_start_time
-			print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
-		except botocore.exceptions.ClientError as e:
-			printfail(method,e.response)
-		except botocore.vendored.requests.exceptions.ReadTimeout as e:
-			#if we get a timeout, retry until we hit max_retries
-
-			print "got a timeout for %s: %s" %(objKey,e)
-			local_elapsed_time = time.time() - local_start_time
-			print "this timeout took %s , initiating retry" %(local_elapsed_time)
-
-			for attempt in range(max_retries):
-				print "attempt #%s of %s" %(attempt + 1,max_retries)
-				try:
-					response = s3client.put_object(
-					Body=b'xyz',
-					Bucket=bucket,
-					Key=objKey)
-					#printsuccess(method,response)
-					local_elapsed_time = time.time() - local_start_time
-					print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
-					break
-				except botocore.vendored.requests.exceptions.ReadTimeout as e:
-					if attempt < (max_retries - 1):
-						print "attempt %s failed, %s remaining" %(attempt + 1, max_retries - attempt)
-						continue
-					else:
-						print "all attempts failed..aborting this transfer"
-						break
-
-	global_elapsed_time = time.time() - global_start_time
-	print "Done making %s files, it took %s" %(filecount, global_elapsed_time)
-
-
 #
-#use this if you want files that are more than just a few bytes
+#upload & retry
 #
 
-def upload_file(bucket, filecount, fileprefix):
+def upload_file(s3client,bucket, filecount, fileprefix):
 
 	# Setup the parameters for the upload 
 	config = TransferConfig(
-    		multipart_threshold= 20 * 1024 * 1024 * 1024,  # Set threshold to 8GB before using Multipart
+		# Set multipart_threshold to 8GB before using Multipart, effectively disabling it
+    		multipart_threshold= 20 * 1024 * 1024 * 1024,  
     		max_concurrency=10,
 	)
 
@@ -272,40 +280,62 @@ def upload_file(bucket, filecount, fileprefix):
 
 
 	method = 'upload_file'
+	#start a timer so we know how long this is all taking
 	global_start_time = time.time()
+
+	#loop through until we've uploaded <filecount> files
 	for i in range(filecount):
+		#measure how long each file takes
 		local_start_time = time.time()
 		nowtime = time.strftime("%Y-%m-%d %H:%M:%S")
-		objKey = fileprefix + '-' + str(i) + '-' + (''.join(choice(ascii_uppercase) for i in range(6))) + '.2mb'
+		#make a random name for each object.
+		objKey = fileprefix + '-' + str(i) + '-' + (''.join(choice(ascii_uppercase) for i in range(6))) + '.file'
 
 		try:
 			response = transfer.upload_file(
-				'/tmp/2mb.file', 
+				input_file, 
 				bucket,
 				objKey
 				)
-			#printsuccess(method,response)
 			local_elapsed_time = time.time() - local_start_time
 			print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
+			#
+			#first exception will trap things like auth, no such bucket, etc
+			#
 		except botocore.exceptions.ClientError as e:
 			printfail(method,e.response)
+
+			#
+			#these 3 exceptions trap various timeout errors.
+			#
 		except (botocore.vendored.requests.exceptions.ConnectionError, 
 			botocore.vendored.requests.exceptions.ReadTimeout,
 			botocore.exceptions.EndpointConnectionError) as e:
+			#
 			#if we get a timeout, retry until we hit max_retries
-			print "got a timeout for %s: %s" %(objKey,e)
+			#
+
+			logger.error('got a timeout for %s -> %s', objKey, e)
+			
+			#
+			#calculate and print how long it took for this exception to be raised
+			# note: this is how you will know what impact changing read_timeout & session_timeout has
+			#
 			local_elapsed_time = time.time() - local_start_time
 			print "this timeout took %s , initiating retry" %(local_elapsed_time)
 
+			#
+			#retry until we either succeed, or reach max_retries
+			#
 			for attempt in range(max_retries):
 				print "attempt #%s of %s" %(attempt + 1,max_retries)
 				try:
 					response =  transfer.upload_file(
-						'/tmp/2mb.file', 
+						input_file, 
 						bucket,
 						objKey
 						)
-					#printsuccess(method,response)
+				
 					local_elapsed_time = time.time() - local_start_time
 					print "at %s made file # %s in %s" %(nowtime, i, local_elapsed_time)
 					break
@@ -313,17 +343,18 @@ def upload_file(bucket, filecount, fileprefix):
 						botocore.vendored.requests.exceptions.ReadTimeout,
 						botocore.exceptions.EndpointConnectionError) as e:
 					if attempt < (max_retries - 1):
-						print "attempt %s failed, %s remaining" %(attempt + 1, max_retries - attempt)
+						logger.error('attempt %s for %s failed with %s, %s remaining', attempt + 1, objKey,e,max_retries - attempt)
 						continue
 					else:
-						print "all attempts failed..aborting this transfer"
+						logger.error('All %s attempts for %s failed, aborting this file',max_retries,objKey)
 						break
 
 	global_elapsed_time = time.time() - global_start_time
 	print "Done making %s files, it took %s" %(filecount, global_elapsed_time)
 
 
-def delete_object(bucket,objKey):
+
+def delete_object(s3client,bucket,objKey):
 	method = 'delete_object'
 	
 	try:
@@ -337,76 +368,15 @@ def delete_object(bucket,objKey):
 		printfail(method,e.response)
 
 
-#
-#right now this won't work, need to use the one-at-a-time method
-#
-
-def delete_objects(cName,objList):
-	method = 'delete_objects'
-
-	objList['Contents'][0]['Key']
-	
-	try:
-		response = s3client.delete_objects(
-		    Bucket=cName,
-		    Delete={
-		        'Objects': [
-		            {
-		                'Key': objList['Contents'][0]['Key']
-		            }
-		        ],
-		        'Quiet': False
-		    },
-		)
-		printsuccess(method,response)
-	except botocore.exceptions.ClientError as e:
-		printfail(method,e.response)
-
 
 
 #########
 #end of functions
 #########
 
-s3client = make_session()
 
-if delete_only == True:
-	print "deleting all files starting with %s/%s" %(bucket,fileprefix)
-	objList =  list_objects(bucket, fileprefix)
-	count = 0
-	for page in objList:
-		for key in page['Contents']:
-			delete_object(bucket,key['Key'])
-			count += 1
-	print "deleted %s" %(count)
-else:
-
-	print "putting %s files to %s"  %(filecount, bucket)
-
-	if largeFiles == True:
-		upload_file(bucket, filecount, fileprefix)
-
-	else:	
-		put_object_lots(bucket, filecount, fileprefix)
-
-	print "finished putting %s files" %(filecount)
-
-	#
-	#get a list of objects in pages, then blow them away
-	#
-
-	objList =  list_objects(bucket, fileprefix)
-
-	count = 0
-	for page in objList:
-			for key in page['Contents']:
-				delete_object(bucket,key['Key'])
-				count += 1
-
-
-	print "deleted %s" %(count)
-
-
+if __name__ == '__main__':
+	main()
 
 
 
