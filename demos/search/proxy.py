fc49985d70f9cf25f49548c3afd668712082d003
Andy Pernsteiner <andypern@gmail.com>
2016-06-21 18:11:45 -0400

    put search demo in its own place

--- demos/search/proxy.py
+++ demos/search/proxy.py
@@ -1,18 +1,60 @@
 #!/usr/bin/python
-# This is a simple port-forward / proxy, written using only the default python
-# library. If you want to make a suggestion or fix something you can contact-me
-# at voorloop_at_gmail.com
-# Distributed over IDC(I Don't Care) license
+# This script starts a proxy which can relay to an iggy box.  The goal is to intercept
+# PUT requests , extract metadata, and populate an ES index.  This is for demo purposes only, and
+# will have LOTS of race conditions, data inconsistencies, etc.
+#
+#original proxy code borrowed from voorloop_at_gmail.com
+
+
+
+
 import socket
 import select
 import time
 import sys
+import httplib
+
+#
+#http request parsing class, taken from http://goo.gl/Q96AKg 
+#
+from BaseHTTPServer import BaseHTTPRequestHandler
+from StringIO import StringIO
+
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = StringIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+
+    def send_error(self, code, message):
+        self.error_code = code
+        self.error_message = message
+
+
+#
+# also need to parse responses, from http://goo.gl/opYG4P
+#
+
+from httplib import HTTPResponse
+
+#
+#we sort of have to trick things here by creating a fake socket to pass our string into
+# 
+
+class FakeSocket():
+    def __init__(self, response_str):
+        self._file = StringIO(response_str)
+    def makefile(self, *args, **kwargs):
+        return self._file
+
+
 
 # Changing the buffer_size and delay, you can improve the speed and bandwidth.
 # But when buffer get to high or delay go too down, you can broke things
 buffer_size = 4096
 delay = 0.0001
-forward_to = ('smtp.zaz.ufsk.br', 25)
+forward_to = ('demo.iggy.bz', 7070)
 
 class Forward:
     def __init__(self):
@@ -58,18 +100,19 @@ class TheServer:
         forward = Forward().start(forward_to[0], forward_to[1])
         clientsock, clientaddr = self.server.accept()
         if forward:
-            print clientaddr, "has connected"
+            #print clientaddr, "has connected"
             self.input_list.append(clientsock)
             self.input_list.append(forward)
             self.channel[clientsock] = forward
             self.channel[forward] = clientsock
+
         else:
             print "Can't establish connection with remote server.",
             print "Closing connection with client side", clientaddr
             clientsock.close()
 
     def on_close(self):
-        print self.s.getpeername(), "has disconnected"
+        #print self.s.getpeername(), "has disconnected"
         #remove objects from input_list
         self.input_list.remove(self.s)
         self.input_list.remove(self.channel[self.s])
@@ -84,8 +127,41 @@ class TheServer:
 
     def on_recv(self):
         data = self.data
+        #print dir(self)
         # here we can parse and/or modify the data before send forward
-        print data
+        
+        #parse the request/response
+        http_request = HTTPRequest(data)
+
+        #We only care about requests for our purposes
+        # in reality, we should only care about requests that get responded to positively
+        # that  might be hard to do though.
+
+        # if there's no command..its a response (not a request)
+        if http_request.command != None:
+            #
+            #we only want to see PUT's here
+            #
+            if http_request.command in "PUT":
+                print http_request.path
+                print http_request.headers['content-length']
+                print http_request.headers.keys()
+        else:
+            #this is a response
+            socket_string = FakeSocket(data)
+            http_response = HTTPResponse(socket_string)
+            try:
+                http_response.begin()
+                #
+                #if the response is just an HTTP continuation ack, ignore it
+                #
+                if len(http_response.getheaders()) > 0:
+                    #print http_response.getheaders()
+                    isResponse = True
+            except httplib.BadStatusLine as e:
+                isHeader = False
+                #print "this isn't a header, skippping"
+
         self.channel[self.s].send(data)
 
 if __name__ == '__main__':
