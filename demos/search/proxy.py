6c59e0caa6d9484f727efd3c98bd7eb90046cfbe
Andy Pernsteiner <andypern@gmail.com>
2016-06-22 15:42:10 -0400

    added CLI args

--- demos/search/proxy.py
+++ demos/search/proxy.py
@@ -6,12 +6,11 @@
 #original proxy code borrowed from voorloop_at_gmail.com
 
 ######TODO
-# * add CLI arg's
 # * figure out crtl-c to kill all threads
-# * detect 'ifdir' or something
+# * detect 'ifdir' or something (no need: content-type works)
 # * typing before insertion? (content-length = int?)<-necessary?
 
-
+import argparse
 import socket
 import select
 import time
@@ -27,6 +26,12 @@ from multiprocessing import Process, JoinableQueue, Queue
 #variables
 #
 threadcount = 5
+
+# Changing the buffer_size and delay, you can improve the speed and bandwidth.
+# But when buffer get to high or delay go too down, you can broke things
+
+buffer_size = 4096
+delay = 0.0001
 #
 #http request parsing class, taken from http://goo.gl/Q96AKg 
 #
@@ -75,13 +80,33 @@ class Ddict(dict):
             self[key] = self.default()
         return dict.__getitem__(self, key)
 
+def getArgs():
+
+    parser = argparse.ArgumentParser()
+
+    script = os.path.basename(sys.argv[0])
+    usage = 'Usage: ' + script + '-l listen_port -e endpoint -c es_cluster -i index -t es_type -n numThreads -v verbose'
+
+    parser.add_argument("-l", "--listen", dest="listen_port", default=9090)
+    parser.add_argument("-e", "--endpoint", dest="endpoint", required=True)
+    parser.add_argument("-c", "--cluster", dest="es_cluster", required=True)
+    parser.add_argument("-i", "--index", dest="index_name", default="demo")
+    parser.add_argument("-t", "--type", dest="es_type", default="fsindex")
+    parser.add_argument("-n", "--num-threads", type=int, dest="num_threads", default=5)
+    parser.add_argument("-v", "--verbose", dest="verbosity", default=False)
 
 
-# Changing the buffer_size and delay, you can improve the speed and bandwidth.
-# But when buffer get to high or delay go too down, you can broke things
-buffer_size = 4096
-delay = 0.0001
-forward_to = ('demo.iggy.bz', 7070)
+    args = parser.parse_args()
+    
+    if (args.endpoint is None): 
+        parser.print_help()
+        sys.exit(1)
+    else: 
+        forward_to = args.endpoint.split(":")
+
+    return args.listen_port,forward_to, args.es_cluster, args.index_name, args.es_type, args.num_threads
+
+
 
 class Forward:
     def __init__(self):
@@ -99,11 +124,14 @@ class TheServer:
     input_list = []
     channel = {}
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, forward_to):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((host, port))
         self.server.listen(200)
+        self.forward_to = forward_to
+
+        print self.forward_to
 
     def main_loop(self):
         self.input_list.append(self.server)
@@ -124,7 +152,7 @@ class TheServer:
                     self.on_recv()
 
     def on_accept(self):
-        forward = Forward().start(forward_to[0], forward_to[1])
+        forward = Forward().start(self.forward_to[0], int(self.forward_to[1]))
         clientsock, clientaddr = self.server.accept()
         if forward:
             #print clientaddr, "has connected"
@@ -174,10 +202,13 @@ class TheServer:
 
 class indexer(Process):
 
-    def __init__(self,file_queue,result_queue):
+    def __init__(self,file_queue,result_queue,es_cluster,index_name,es_type):
         Process.__init__(self)
         self.file_queue = file_queue
         self.result_queue = result_queue
+        self.es_cluster = es_cluster
+        self.index_name = index_name
+        self.es_type = es_type
         
 
 
@@ -190,10 +221,9 @@ class indexer(Process):
         #
 
         
-        INDEX = 'demo'
-        TYPE = 'fstest2'
+
         es = Elasticsearch(
-            ['192.168.2.147'],
+            [self.es_cluster],
             port=9200
         )
 
@@ -206,7 +236,7 @@ class indexer(Process):
             #print phat_hash
 
             index_id = phat_hash['path']
-            res = es.index(index=INDEX, doc_type=TYPE, id=index_id, body=phat_hash)
+            res = es.index(index=self.index_name, doc_type=self.es_type, id=index_id, body=phat_hash)
             print res
 
             self.result_queue.put(res)
@@ -270,14 +300,17 @@ def header_handler(http_data):
 
 
 if __name__ == '__main__':
-    server = TheServer('', 9090)
+
+    listen_port,forward_to, es_cluster, index_name, es_type, num_threads = getArgs()
+
+    server = TheServer('', listen_port, forward_to)
     #
     #launch processes for doing work
     #
     file_queue = JoinableQueue()
     result_queue = Queue()
-    file_processes = [ indexer(file_queue, result_queue)
-                      for i in range(threadcount) ]    
+    file_processes = [ indexer(file_queue, result_queue,es_cluster,index_name,es_type)
+                      for i in range(num_threads) ]    
 
 
 
